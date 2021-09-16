b5e64f906f9078f188a79fa59dabd5bf973cf2c7
beatrixtam <88252336+beatrixtam@users.noreply.github.com>
2021-09-16 12:56:54 +0100

    Updating time and date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+
+	let now = `${day} ${hours}:${minutes}`;
+
+	return now;
+}
+
 function showTemperature(response) {
 	console.log(response.data);
 	let temperatureElement = document.querySelector("#temp-now");
@@ -14,6 +34,9 @@ function showTemperature(response) {
 
 	let windElement = document.querySelector("#wind");
 	windElement.innerHTMAL = Math.round(response.data.wind.speed);
+
+	let dateElement = document.querySelector("#date-time");
+	dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "ca40b820105beb53b92d32a2aebf57bb";
